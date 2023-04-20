6b77545b45bfae8fb3ee73f708de32388d4fbcf7
Ricardonovais1 <ricardonovais1@gmail.com>
2023-04-19 21:53:43 -0300

    Teste 3 (sem nenhum galpão)

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -10,4 +10,38 @@ describe 'usuario visita tela inicial' do
         # Assert
         expect(page).to have_content('Galpões & Estoque')
     end
+
+    it 'e vê os galpões cadastrados' do 
+        # Arrange 
+        # Cadastrar 2 galpões: Rio e Maceió
+        Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+        Warehouse.create(name: 'Maceió', code: 'MCZ', city: 'Maceió', area: 50_000)
+
+
+        # Act 
+        visit('/')
+
+        # Assert
+        expect(page).to have_content('Rio')
+        expect(page).to have_content('Código: SDU')
+        expect(page).to have_content('Cidade: Rio de Janeiro')
+        expect(page).to have_content('Área: 60000 m²')
+
+        expect(page).to have_content('Maceió')
+        expect(page).to have_content('Código: MCZ')
+        expect(page).to have_content('Cidade: Maceió')
+        expect(page).to have_content('Área: 50000 m²')
+
+    end
+
+    it 'e não existem galpões cadastrados' do
+        # Arrange
+
+
+        # Act
+        visit('/')
+
+        # Assert
+        expect(page).to have_content('Nenhum galpão cadastrado')
+    end
 end
