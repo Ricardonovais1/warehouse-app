722966f764e29a4db4c8a079eb40b7db85d2c031
Ricardonovais1 <ricardonovais1@gmail.com>
2023-04-26 09:20:35 -0300

    Cadastro de Fornecedores finalizado

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -3,26 +3,172 @@ require 'rails_helper'
 RSpec.describe Supplier, type: :model do
   describe '#valid?' do 
     context 'presence' do 
-    it 'false when corporate_name is empty' do
-      # # Arrange 
-      # s = Supplier.new(corporate_name: '', 
-      #                 brand_name: 'Amigo Violão', 
-      #                 registration_number: 12345, 
-      #                 full_address: 'Rua das Palmeiras, 344', 
-      #                 city: 'Belo Horizonte',
-      #                 state: 'Minas Gerais', 
-      #                 email: 'av@av.com')
-      # s.save()
+      it 'false when corporate_name is empty' do
+        # Arrange 
+        s = Supplier.new(corporate_name: '', 
+                        brand_name: 'Amigo Violão', 
+                        registration_number: 12345, 
+                        full_address: 'Rua das Palmeiras, 344', 
+                        city: 'Belo Horizonte',
+                        state: 'Minas Gerais', 
+                        email: 'av@av.com')
 
-      # # Act
-      # visit root_path 
-      # within('nav') do 
-      #   click_on 'Fornecedores'
-      # end
+        # Act
+        result = s.valid?
 
-      # # Assert
+        # Assert
+        expect(result).to eq false
+      end
+      
+      it 'false when brand_name is empty' do
+        # Arrange 
+        s = Supplier.new(corporate_name: 'AV Guitars and Accessories', 
+                        brand_name: '', 
+                        registration_number: 12345, 
+                        full_address: 'Rua das Palmeiras, 344', 
+                        city: 'Belo Horizonte',
+                        state: 'Minas Gerais', 
+                        email: 'av@av.com')
 
+        # Act
+        result = s.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'false when registration_number is empty' do
+        # Arrange 
+        s = Supplier.new(corporate_name: 'AV Guitars and Accessories', 
+                        brand_name: 'Amigo Violão', 
+                        registration_number: '', 
+                        full_address: 'Rua das Palmeiras, 344', 
+                        city: 'Belo Horizonte',
+                        state: 'Minas Gerais', 
+                        email: 'av@av.com')
+
+        # Act
+        result = s.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'false when full_address is empty' do
+        # Arrange 
+        s = Supplier.new(corporate_name: 'AV Guitars and Accessories', 
+                        brand_name: 'Amigo Violão', 
+                        registration_number: 12345, 
+                        full_address: '', 
+                        city: 'Belo Horizonte',
+                        state: 'Minas Gerais', 
+                        email: 'av@av.com')
+
+        # Act
+        result = s.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+      
+      it 'false when city is empty' do
+        # Arrange 
+        s = Supplier.new(corporate_name: 'AV Guitars and Accessories', 
+                        brand_name: 'Amigo Violão', 
+                        registration_number: 12345, 
+                        full_address: 'Rua das Palmeiras, 344', 
+                        city: '',
+                        state: 'Minas Gerais', 
+                        email: 'av@av.com')
+
+        # Act
+        result = s.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'false when state is empty' do
+        # Arrange 
+        s = Supplier.new(corporate_name: 'AV Guitars and Accessories', 
+                        brand_name: 'Amigo Violão', 
+                        registration_number: 12345, 
+                        full_address: 'Rua das Palmeiras, 344', 
+                        city: 'Belo Horizonte',
+                        state: '', 
+                        email: 'av@av.com')
+
+        # Act
+        result = s.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'false when email is empty' do
+        # Arrange 
+        s = Supplier.new(corporate_name: 'AV Guitars and Accessories', 
+                        brand_name: 'Amigo Violão', 
+                        registration_number: 12345, 
+                        full_address: 'Rua das Palmeiras, 344', 
+                        city: 'Belo Horizonte',
+                        state: 'Minas Gerais', 
+                        email: '')
+
+        # Act
+        result = s.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
     end
+
+    context 'uniqueness' do
+        it 'false when registration_number is already in use' do 
+          # Arrange
+          first_supplier = Supplier.create(corporate_name: 'Souza comércio de roupas e acessórios LTDA', 
+                                           brand_name: 'Magazine Souza', 
+                                           registration_number: 12345, 
+                                           full_address: 'Rua Lago das Araras, 1000', 
+                                           city: 'Ji Paraná',
+                                           state: 'Rondônia', 
+                                           email: 'souza@souza.com.br')
+            second_supplier = Supplier.new(corporate_name: 'AV Guitars and Accessories', 
+                                           brand_name: 'Amigo Violão', 
+                                           registration_number: 12345, 
+                                           full_address: 'Rua das Palmeiras, 344', 
+                                           city: 'Belo Horizonte',
+                                           state: 'Minas Gerais', 
+                                           email: 'av@av.com')
+          # Act
+          result = second_supplier.valid?
+
+          # Assert
+          expect(result).to eq false
+        end
+
+        it 'false when email is already in use' do 
+          # Arrange
+          first_supplier = Supplier.create(corporate_name: 'Souza comércio de roupas e acessórios LTDA', 
+                                           brand_name: 'Magazine Souza', 
+                                           registration_number: 54321, 
+                                           full_address: 'Rua Lago das Araras, 1000', 
+                                           city: 'Ji Paraná',
+                                           state: 'Rondônia', 
+                                           email: 'av@av.com')
+            second_supplier = Supplier.new(corporate_name: 'AV Guitars and Accessories', 
+                                           brand_name: 'Amigo Violão', 
+                                           registration_number: 12345, 
+                                           full_address: 'Rua das Palmeiras, 344', 
+                                           city: 'Belo Horizonte',
+                                           state: 'Minas Gerais', 
+                                           email: 'av@av.com')
+          # Act
+          result = second_supplier.valid?
+
+          # Assert
+          expect(result).to eq false
+        end
     end
   end
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,105 +1,106 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
-    describe '#valid?' do 
-        context 'presence' do 
-        it 'false when name is empty' do
-        # Arrange 
-        warehouse = Warehouse.new(name: '', code: 'RIO', address: 'Endereço', 
-                                      cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição')
+  describe '#valid?' do 
+    context 'presence' do 
+    it 'false when name is empty' do
+    # Arrange 
+    warehouse = Warehouse.new(name: '', code: 'RIO', address: 'Endereço', 
+                                    cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição')
 
-        # Act
-        result = warehouse.valid?
+    # Act
+    result = warehouse.valid?
 
-        # Assert
-        expect(result).to eq false
-        end
-
-        it 'false when code is empty' do
-        # Arrange 
-        warehouse = Warehouse.new(name: 'Rio galpão', code: '', address: 'Endereço', 
-                                        cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição')
-
-        # Act
-        result = warehouse.valid?
-
-        # Assert
-        expect(result).to eq false
-        end
-
-        it 'false when address is empty' do
-        # Arrange 
-        warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: '', 
-                                        cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição')
-
-        # Act
-        result = warehouse.valid?
-
-        # Assert
-        expect(result).to eq false
-        end
-
-        it 'false when cep is empty' do
-        # Arrange 
-        warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
-                                        cep: '', city: 'Rio', area: 1000, description: 'Alguma descrição')
-
-        # Act
-        result = warehouse.valid?
-
-        # Assert
-        expect(result).to eq false
-        end
-
-        it 'false when city is empty' do
-        # Arrange 
-        warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
-                                        cep: '25000-000', city: '', area: 1000, description: 'Alguma descrição')
-
-        # Act
-        result = warehouse.valid?
-
-        # Assert
-        expect(result).to eq false
-        end
-
-        it 'false when area is empty' do
-        # Arrange 
-        warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
-                                        cep: '25000-000', city: 'Cidade', area: '', description: 'Alguma descrição')
-
-        # Act
-        result = warehouse.valid?
-
-        # Assert
-        expect(result).to eq false
-        end
-
-        it 'false when description is empty' do
-        # Arrange 
-        warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
-                                        cep: '25000-000', city: 'Cidade', area: '', description: '')
-
-        # Act
-        result = warehouse.valid?
-
-        # Assert
-        expect(result).to eq false
-        end
+    # Assert
+    expect(result).to eq false
     end
-    context ''
-        it 'false when code is already in use' do 
-        #Arrange
-        first_warehouse = Warehouse.create(name: 'SP galpão', code: 'RIO', address: 'Endereço 2', 
-                                        cep: '25000-999', city: 'Município', area: '9000', description: 'Hello')
-        second_warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
-                                        cep: '25000-000', city: 'Cidade', area: '1000', description: 'Olá')
 
-        # Act 
-        result = second_warehouse.valid?
+    it 'false when code is empty' do
+    # Arrange 
+    warehouse = Warehouse.new(name: 'Rio galpão', code: '', address: 'Endereço', 
+                                    cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição')
 
-        # Assert
-        expect(result).to eq false
-        end
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
     end
+
+    it 'false when address is empty' do
+    # Arrange 
+    warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: '', 
+                                    cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição')
+
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+    end
+
+    it 'false when cep is empty' do
+    # Arrange 
+    warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
+                                    cep: '', city: 'Rio', area: 1000, description: 'Alguma descrição')
+
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+    end
+
+    it 'false when city is empty' do
+    # Arrange 
+    warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
+                                    cep: '25000-000', city: '', area: 1000, description: 'Alguma descrição')
+
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+    end
+
+    it 'false when area is empty' do
+    # Arrange 
+    warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
+                                    cep: '25000-000', city: 'Cidade', area: '', description: 'Alguma descrição')
+
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+    end
+
+    it 'false when description is empty' do
+    # Arrange 
+    warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
+                                    cep: '25000-000', city: 'Cidade', area: '', description: '')
+
+    # Act
+    result = warehouse.valid?
+
+    # Assert
+    expect(result).to eq false
+    end
+    end
+    context 'uniqueness' do
+      it 'false when code is already in use' do 
+      #Arrange
+      first_warehouse = Warehouse.create(name: 'SP galpão', code: 'RIO', address: 'Endereço 2', 
+                                      cep: '25000-999', city: 'Município', area: '9000', description: 'Hello')
+      second_warehouse = Warehouse.new(name: 'Rio galpão', code: 'RIO', address: 'Endereço', 
+                                      cep: '25000-000', city: 'Cidade', area: '1000', description: 'Olá')
+
+      # Act 
+      result = second_warehouse.valid?
+
+      # Assert
+      expect(result).to eq false
+      end
+    end
+  end
 end
